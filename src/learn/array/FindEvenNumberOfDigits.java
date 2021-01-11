241eb0e1748929e0fff2ca6f5ebc056e8f851517
taowu750 <wutaodsg5201314@gmail.com>
2021-01-11 17:17:20 +0800

    :sparkles: 新增 FindEvenNumberOfDigits.java

---
+++ src/learn/array/FindEvenNumberOfDigits.java
@@ -0,0 +1,50 @@
+package learn.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * 给定一个整数数组 nums，返回十进制位数为偶数的整数的个数。
+ * <p>
+ * 例子：
+ * Input: nums = [12,345,2,6,7896]
+ * Output: 2
+ * Explanation:
+ * 12 包含 2 个数字
+ * 345 包含 3 个数字
+ * 2 包含 1 个数字
+ * 6 包含 1 个数字
+ * 7896 包含 4 个数字
+ * 因此只有 12 和 7896 有偶数个数字
+ * <p>
+ * 注意：
+ * - 1 <= nums.length <= 500
+ * - 1 <= nums[i] <= 10^5
+ */
+public class FindEvenNumberOfDigits {
+
+    public int findNumbers(int[] nums) {
+        int cnt = 0;
+        for (int n : nums) {
+            int curCnt = 0;
+            while (n != 0) {
+                curCnt++;
+                n /= 10;
+            }
+            if ((curCnt & 1) == 0)
+                cnt++;
+        }
+
+        return cnt;
+    }
+
+    @Test
+    public void testFindNumbers() {
+        int[] a = {12, 345, 2, 6, 7896};
+        assertEquals(findNumbers(a), 2);
+
+        int[] a2 = {555, 901, 482, 1771};
+        assertEquals(findNumbers(a2), 1);
+    }
+}
