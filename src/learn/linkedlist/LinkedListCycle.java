c356618d72bb8d42b04211dfd5d1dfa3d2b55a24
taowu750 <wutaodsg5201314@gmail.com>
2021-01-22 15:59:49 +0800

    :sparkles: 新增“旋转链表”题解；:memo: 修改 LinkedListCycle.ListNode 定义

--- src/learn/linkedlist/LinkedListCycle.java
+++ src/learn/linkedlist/LinkedListCycle.java
@@ -40,8 +40,15 @@ public class LinkedListCycle {
         int val;
         ListNode next;
 
+        public ListNode() {}
+
         public ListNode(int val) {
             this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
         }
     }
 
