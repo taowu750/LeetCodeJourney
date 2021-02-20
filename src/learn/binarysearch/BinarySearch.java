f92f38ad4ab6a564e926bbdbdabb9c9269858786
taowu750 <wutaodsg5201314@gmail.com>
2021-02-20 15:17:02 +0800

    :memo: 修改“二分查找”题解，增加循环不变式说明

--- src/learn/binarysearch/BinarySearch.java
+++ src/learn/binarysearch/BinarySearch.java
@@ -41,6 +41,26 @@ public class BinarySearch {
         assertEquals(method.search(new int[]{1}, -1), -1);
     }
 
+    /**
+     * 循环不变式：
+     * - 如果 target 存在初始数组 [0, n-1]，那么它一定在 [lo, hi] 中。
+     *
+     * 初始化：
+     * - 第一轮循环开始之前，处理的数组就是原始数组，这时显然成立。
+     *
+     * 保持：
+     * - 对于 array[mid]==target，查找到了 target 对应的下标，直接返回。
+     * - 对于 array[mid] < target，array[lo, ..., mid] 均小于 target，
+     *   target 只可能存在于 array[mid+1, ..., hi] 中。
+     * - 对于 array[mid] > target，array[mid, ..., hi] 均大于 target，
+     *   target 只可能存在于 array[lo, ..., mid-1] 中。
+     * - 在前两种情况中，数组长度每次至少减少 1（实际减少的长度分别是 mid-lo+1 和 hi-mid+1），
+     *   直到由1（lo==hi）变为0（lo>hi），不会发生死循环。
+     *
+     * 终止：
+     * - 结束时，lo > hi，待处理数组为空，表示 target 不存在于所有步骤的待处理数组，
+     *   再结合每一步排除的部分数组中也不可能有 target，因此 target 不存在于原数组。
+     */
     public int search(int[] nums, int target) {
         int lo = 0, hi = nums.length - 1;
         while (lo <= hi) {
