88146d0ee4d786a278d4beb44b210d19ff0b1a5c
taowu750 <wutaodsg5201314@gmail.com>
2021-05-02 17:14:24 +0800

    :hammer: 修改 TreeNode，使其更易于测试。

--- src/learn/binarytree/TreeNode.java
+++ src/learn/binarytree/TreeNode.java
@@ -2,10 +2,14 @@ package learn.binarytree;
 
 import util.datastructure.BinaryTreeNode;
 
+import java.util.Objects;
+
 /**
  * 二叉树结点，和 LeetCode 的二叉树结点名称保持一致
  */
-public class TreeNode extends BinaryTreeNode<TreeNode> {
+public class TreeNode extends BinaryTreeNode<TreeNode> implements Comparable<TreeNode> {
+
+    private Integer compareVal;
 
     public TreeNode() {
     }
@@ -29,10 +33,24 @@ public class TreeNode extends BinaryTreeNode<TreeNode> {
     }
 
     @Override
+    public int hashCode() {
+        return Objects.hash(val, this.left != null ? this.left.hashCode() : 0,
+                this.right != null ? this.right.hashCode() : 0);
+    }
+
+    @Override
     public String toString() {
-        return "TreeNode(" +
-                + val +
-                ')';
+        return "(" + val + ')';
+    }
+
+    @Override
+    public int compareTo(TreeNode o) {
+        // 仅测试用，所以 compareVal 只计算一次不再更新
+        if (compareVal == null)
+            compareVal = hashCode();
+        if (o.compareVal == null)
+            o.compareVal = o.hashCode();
+        return Integer.compare(compareVal, o.compareVal);
     }
 
     public static TreeNode newTree(Integer... vals) {
