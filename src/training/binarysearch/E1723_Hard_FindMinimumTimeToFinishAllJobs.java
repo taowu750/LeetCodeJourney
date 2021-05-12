4b44d2a356d493af9e4b04631ff985e7f2ab2469
taowu750 <wutaodsg5201314@gmail.com>
2021-05-12 15:49:38 +0800

    :memo: 修改“1723.[Hard] 完成所有工作的最短时间”题解，增加算法思路来源链接。

--- src/training/binarysearch/E1723_Hard_FindMinimumTimeToFinishAllJobs.java
+++ src/training/binarysearch/E1723_Hard_FindMinimumTimeToFinishAllJobs.java
@@ -45,6 +45,9 @@ public class E1723_Hard_FindMinimumTimeToFinishAllJobs {
     /**
      * 二分查找结合回溯法。
      *
+     * 思路来源于：
+     * https://leetcode-cn.com/problems/find-minimum-time-to-finish-all-jobs/solution/wan-cheng-suo-you-gong-zuo-de-zui-duan-s-hrhu/
+     *
      * LeetCode 耗时：1 ms - 99.65%
      *          内存消耗：35.6 MB - 86.97%
      */
@@ -85,8 +88,8 @@ public class E1723_Hard_FindMinimumTimeToFinishAllJobs {
      *
      * 每次尝试将任务 i 分配给工人。我们有以下三个优化：
      * 1. 优先分配工作量大的工作
-     *    感性地理解，如果要求将小石子和大石块放入玻璃瓶中，优先放入大石块更容易使得工作变得简单。
-     *    在搜索过程中，优先分配工作量小的工作会使得工作量大的工作更有可能最后无法被分配。
+     *    感性地理解，如果要求将小石子和大石块放入玻璃瓶中，优先放入大石块更容易使得工作变得简单，
+     *    并且更容易触发剪枝条件。在搜索过程中，优先分配工作量小的工作会使得工作量大的工作更有可能最后无法被分配。
      * 2. 当工人 j 还没被分配工作时，我们不给工人 j+1 分配工作。
      *    如果当前工人 j 和 j+1 都没有被分配工作，那么我们将工作先分配给任何一个人都没有区别;
      *    如果分配给工人 j 不能成功完成分配任务，那么分配给工人 j+1 也一样无法完成。
