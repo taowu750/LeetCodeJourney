251df744af39e501055c1189412735526354d347
taowu750 <wutaodsg5201314@gmail.com>
2021-07-17 10:24:21 +0800

    :sparkles: 新增“11.[Medium] 盛最多水的容器”题解：双指针法。

---
+++ src/training/array/E11_Medium_ContainerWithMostWater.java
@@ -0,0 +1,73 @@
+package training.array;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.function.ToIntFunction;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * 11. 盛最多水的容器: https://leetcode-cn.com/problems/container-with-most-water/
+ *
+ * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，
+ * 垂直线 i 的两个端点分别为 (i,ai) 和 (i, 0) 。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ *
+ * 例 1：
+ * 输入：[1,8,6,2,5,4,8,3,7]
+ * 输出：49
+ * 解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+ *
+ * 例 2：
+ * 输入：height = [1,1]
+ * 输出：1
+ *
+ * 例 3：
+ * 输入：height = [4,3,2,1,4]
+ * 输出：16
+ *
+ * 例 4：
+ * 输入：height = [1,2,1]
+ * 输出：2
+ *
+ * 约束：
+ * - n = height.length
+ * - 2 <= n <= 3 * 10^4
+ * - 0 <= height[i] <= 3 * 10^4
+ */
+public class E11_Medium_ContainerWithMostWater {
+
+    static void test(ToIntFunction<int[]> method) {
+        assertEquals(49, method.applyAsInt(new int[]{1,8,6,2,5,4,8,3,7}));
+        assertEquals(1, method.applyAsInt(new int[]{1,1}));
+        assertEquals(16, method.applyAsInt(new int[]{4,3,2,1,4}));
+        assertEquals(2, method.applyAsInt(new int[]{1,2,1}));
+    }
+
+    /**
+     * 双指针法。
+     *
+     * LeetCode 耗时：4ms - 84%
+     *          内存消耗：53.2MB - 47%
+     */
+    public int maxArea(int[] height) {
+        int max = 0;
+        for (int i = 0, j = height.length - 1; i < j;) {
+            max = Math.max(max, Math.min(height[i], height[j]) * (j - i));
+            if (height[i] > height[j]) {
+                j--;
+            } else if (height[i] < height[j]) {
+                i++;
+            } else {
+                i++;
+                j--;
+            }
+        }
+
+        return max;
+    }
+
+    @Test
+    public void testMaxArea() {
+        test(this::maxArea);
+    }
+}
