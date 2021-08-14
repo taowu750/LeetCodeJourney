9c8f40b4da7b843d880bf47c587e290548310f8f
taowu750 <wutaodsg5201314@gmail.com>
2021-08-14 16:05:53 +0800

    :heavy_plus_sign: 修改“E301.[Hard] 删除无效的括号”题解：带有更好剪枝的dfs。

--- src/training/backtracking/E301_Hard_RemoveInvalidParentheses.java
+++ src/training/backtracking/E301_Hard_RemoveInvalidParentheses.java
@@ -14,22 +14,22 @@ import static util.CollectionUtil.equalsIgnoreOrder;
 
 /**
  * 301. 删除无效的括号: https://leetcode-cn.com/problems/remove-invalid-parentheses/
- *
+ * <p>
  * 给你一个由若干括号和字母组成的字符串 s ，删除最小数量的无效括号，使得输入的字符串有效。
  * 返回所有可能的结果。答案可以按「任意顺序」返回。
- *
+ * <p>
  * 例 1：
  * 输入：s = "()())()"
  * 输出：["(())()","()()()"]
- *
+ * <p>
  * 例 2：
  * 输入：s = "(a)())()"
  * 输出：["(a())()","(a)()()"]
- *
+ * <p>
  * 例 3：
  * 输入：s = ")("
  * 输出：[""]
- *
+ * <p>
  * 约束：
  * - 1 <= s.length <= 25
  * - s 由小写英文字母以及括号 '(' 和 ')' 组成
@@ -38,55 +38,45 @@ import static util.CollectionUtil.equalsIgnoreOrder;
 public class E301_Hard_RemoveInvalidParentheses {
 
     static void test(Function<String, List<String>> method) {
-        equalsIgnoreOrder(asList("(a())()","(a)()()"), method.apply("(a)())()"));
-        equalsIgnoreOrder(asList("(())()","()()()"), method.apply("()())()"));
+        equalsIgnoreOrder(asList("(a())()", "(a)()()"), method.apply("(a)())()"));
+        equalsIgnoreOrder(asList("(())()", "()()()"), method.apply("()())()"));
         equalsIgnoreOrder(singletonList(""), method.apply(")("));
         equalsIgnoreOrder(singletonList(""), method.apply("(((("));
         equalsIgnoreOrder(singletonList("abcdef"), method.apply("abcdef"));
-        equalsIgnoreOrder(asList("(())()","()()()"), method.apply("()()))()"));
+        equalsIgnoreOrder(asList("(())()", "()()()"), method.apply("()()))()"));
+        equalsIgnoreOrder(singletonList("()"), method.apply(")()("));
     }
 
     /**
-     * LeetCode 耗时：117 ms - 11.47%
-     *          内存消耗：38.4 MB - 78.31%
+     * LeetCode 耗时：109 ms - 16.34%
+     * 内存消耗：38.4 MB - 78.31%
      */
     public List<String> removeInvalidParentheses(String s) {
-        // 去掉小写字母
-        StringBuilder sb = new StringBuilder(s.length());
+        // 计算需要删除的左括号，右括号数量
+        int leftRemove = 0, rightRemove = 0;
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if (ch == '(' || ch == ')') {
-                sb.append(ch);
-            }
-        }
-        // 计算合法括号序列的长度，算法参见 E32_Hard_LongestValidParentheses
-        int validLen = 0;
-        String parentheses = sb.toString();
-        int[] dp = new int[parentheses.length()];
-        for (int i = 1; i < parentheses.length(); i++) {
-            char c = parentheses.charAt(i);
-            if (c == ')') {
-                if (parentheses.charAt(i - 1) == '(') {
-                    dp[i] = 2 + (i >= 2 ? dp[i - 2] : 0);
-                    validLen += 2;
-                } else if (dp[i - 1] > 0 && dp[i - 1] <= i - 1 && parentheses.charAt(i - 1 - dp[i - 1]) == '(') {
-                    int prev = i - 1 - dp[i - 1] - 1;
-                    dp[i] = 2 + dp[i - 1] + (prev >= 0 ? dp[prev] : 0);
-                    validLen += 2;
+            if (ch == '(') {
+                leftRemove++;
+            } else if (ch == ')') {
+                // 前面有左括号，则右括号可以配对
+                if (leftRemove > 0) {
+                    leftRemove--;
+                } else {
+                    rightRemove++;
                 }
             }
         }
-        // 需要删除的括号的数量
-        int needRemove = dp.length - validLen;
 
-        sb.setLength(0);
+        StringBuilder sb = new StringBuilder(s.length());
         Set<String> result = new HashSet<>();
-        dfs(s, 0, result, sb, needRemove, 0);
+        dfs(s, 0, result, sb, leftRemove, rightRemove, 0);
 
         return new ArrayList<>(result);
     }
 
-    private void dfs(String s, int idx, Set<String> result, StringBuilder sb, int needRemove, int lrDiff) {
+    private void dfs(String s, int idx, Set<String> result, StringBuilder sb,
+                     int leftRemove, int rightRemove, int lrDiff) {
         if (idx >= s.length()) {
             if (lrDiff == 0) {
                 result.add(sb.toString());
@@ -97,24 +87,24 @@ public class E301_Hard_RemoveInvalidParentheses {
         char ch = s.charAt(idx);
         if (ch == '(') {
             sb.append('(');
-            dfs(s, idx + 1, result, sb, needRemove, lrDiff + 1);
+            dfs(s, idx + 1, result, sb, leftRemove, rightRemove, lrDiff + 1);
             sb.deleteCharAt(sb.length() - 1);
-            if (--needRemove >= 0) {
-                dfs(s, idx + 1, result, sb, needRemove, lrDiff);
+            if (--leftRemove >= 0) {
+                dfs(s, idx + 1, result, sb, leftRemove, rightRemove, lrDiff);
             }
         } else if (ch == ')') {
             // 必须保证 ( 数量大于等于 ) 才能构成合法序列
             if (lrDiff - 1 >= 0) {
                 sb.append(')');
-                dfs(s, idx + 1, result, sb, needRemove, lrDiff - 1);
+                dfs(s, idx + 1, result, sb, leftRemove, rightRemove, lrDiff - 1);
                 sb.deleteCharAt(sb.length() - 1);
             }
-            if (--needRemove >= 0) {
-                dfs(s, idx + 1, result, sb, needRemove, lrDiff);
+            if (--rightRemove >= 0) {
+                dfs(s, idx + 1, result, sb, leftRemove, rightRemove, lrDiff);
             }
         } else {
             sb.append(ch);
-            dfs(s, idx + 1, result, sb, needRemove, lrDiff);
+            dfs(s, idx + 1, result, sb, leftRemove, rightRemove, lrDiff);
             sb.deleteCharAt(sb.length() - 1);
         }
     }
@@ -122,5 +112,91 @@ public class E301_Hard_RemoveInvalidParentheses {
     @Test
     public void testRemoveInvalidParentheses() {
         test(this::removeInvalidParentheses);
+    }
+
+
+    /**
+     * 更好的 dfs 方法。
+     *
+     * LeetCode 耗时：2 ms - 97.06%
+     *          内存消耗：38.4 MB - 73.99%
+     */
+    public List<String> betterDfs(String s) {
+        // 计算需要删除的左括号，右括号数量
+        int leftRemove = 0, rightRemove = 0, leftCnt = 0, rightCnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(') {
+                leftRemove++;
+                leftCnt++;
+            } else if (ch == ')') {
+                // 前面有左括号，则右括号可以配对
+                if (leftRemove > 0) {
+                    leftRemove--;
+                } else {
+                    rightRemove++;
+                }
+                rightCnt++;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder(s.length());
+        Set<String> result = new HashSet<>();
+        betterDfs(s, 0, result, sb, 0, 0, leftRemove, rightRemove, leftCnt, rightCnt);
+
+        return new ArrayList<>(result);
+    }
+
+    /**
+     * cnt 表示已添加的括号数量，remove 表示待删除的括号数量，remain 表示 [idx, len) 中剩余的括号数量。
+     */
+    private void betterDfs(String s, int idx, Set<String> result, StringBuilder sb,
+                           int leftCnt, int rightCnt, int leftRemove, int rightRemove, int leftRemain, int rightRemain) {
+        if (idx == s.length()) {
+            result.add(sb.toString());
+            return;
+        }
+
+        char ch = s.charAt(idx);
+        switch (ch) {
+            case '(':
+                // 剪枝操作
+                // 保证剩下的括号数量要足够减，才能添加括号到路径中
+                if (leftRemain - 1 >= leftRemove) {
+                    sb.append(ch);
+                    betterDfs(s, idx + 1, result, sb, leftCnt + 1, rightCnt, leftRemove, rightRemove,
+                            leftRemain - 1, rightRemain);
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+                if (leftRemove >= 1) {
+                    betterDfs(s, idx + 1, result, sb, leftCnt, rightCnt, leftRemove - 1, rightRemove,
+                            leftRemain - 1, rightRemain);
+                }
+                break;
+
+            case ')':
+                // 右括号除了保证剩下的括号数量要足够减，还要保证当前右括号数量小于左括号数量
+                if (rightRemain - 1 >= rightRemove && leftCnt >= rightCnt + 1) {
+                    sb.append(ch);
+                    betterDfs(s, idx + 1, result, sb, leftCnt, rightCnt + 1, leftRemove, rightRemove,
+                            leftRemain, rightRemain - 1);
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+                if (rightRemove >= 1) {
+                    betterDfs(s, idx + 1, result, sb, leftCnt, rightCnt, leftRemove, rightRemove - 1,
+                            leftRemain, rightRemain - 1);
+                }
+                break;
+
+            default:
+                sb.append(ch);
+                betterDfs(s, idx + 1, result, sb, leftCnt, rightCnt, leftRemove, rightRemove, leftRemain, rightRemain);
+                sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+
+    @Test
+    public void testBetterDfs() {
+        test(this::betterDfs);
     }
 }
