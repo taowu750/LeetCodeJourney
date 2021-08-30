369098aebe74ee224f5c9023ddbda3b86fa8ed08
taowu750 <wutaodsg5201314@gmail.com>
2021-08-30 16:24:36 +0800

    :recycle: 修改“31.[Medium] 下一个排列”题解，改善代码结构。

--- src/training/array/E31_Medium_NextPermutation.java
+++ src/training/array/E31_Medium_NextPermutation.java
@@ -2,7 +2,6 @@ package training.array;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.function.Consumer;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
@@ -53,22 +52,32 @@ public class E31_Medium_NextPermutation {
         nums = new int[]{1};
         method.accept(nums);
         assertArrayEquals(new int[]{1}, nums);
+
+        nums = new int[]{1, 2, 3, 2};
+        method.accept(nums);
+        assertArrayEquals(new int[]{1, 3, 2, 2}, nums);
     }
 
     /**
-     * LeetCode 耗时：1 ms - 98.00%
+     * LeetCode 耗时：0 ms - 100%
      *          内存消耗：38.5 MB - 84.63%
      */
     public void nextPermutation(int[] nums) {
-        int i = nums.length - 2;
-        // 从右往左遍历，如果 [i] < [i+1]，则可以通过排序 [i+1..len]，
-        // 然后选择排序后的数字中刚好大于 [i] 的数字与其交换，就能得到下一个排列
-        for (; i >= 0; i--) {
-            int num = nums[i];
-            if (num < nums[i + 1]) {
-                Arrays.sort(nums, i + 1, nums.length);
-                // 二分查找找到刚好大于 nums[i] 的元素。参见“二分查找解析”文档
-                int lo = i + 1, hi = nums.length;
+        int n = nums.length, i = n - 1;
+        // 从右往左遍历，如果 [i-1] < [i]，则可以通过排序 [i..len]，
+        // 然后选择排序后的数字中刚好大于 [i-1] 的数字与其交换，就能得到下一个排列
+        for (; i > 0; i--) {
+            if (nums[i - 1] < nums[i]) {
+                // [i..len] 是逆序的，通过交换可以变为正序的
+                int cnt = (n - i) >>> 1;
+                for (int j = 0; j < cnt; j++) {
+                    int tmp = nums[i + j];
+                    nums[i + j] = nums[n - 1 - j];
+                    nums[n - 1 - j] = tmp;
+                }
+                int num = nums[i - 1];
+                // 二分查找找到刚好大于 nums[i - 1] 的元素。参见“二分查找解析”文档
+                int lo = i, hi = n;
                 while (lo < hi) {
                     int mid = (lo + hi) >>> 1;
                     if (nums[mid] <= num)
@@ -76,14 +85,14 @@ public class E31_Medium_NextPermutation {
                     else
                         hi = mid;
                 }
-                nums[i] = nums[lo];
+                nums[i - 1] = nums[lo];
                 nums[lo] = num;
                 break;
             }
         }
 
         // 如果 nums 是降序排列的，则需要将其反过来
-        if (i < 0) {
+        if (i == 0) {
             int size = nums.length / 2;
             for (int j = 0; j < size; j++) {
                 int tmp = nums[j];
