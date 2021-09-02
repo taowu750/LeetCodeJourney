440cb6d1ca1c96b723b276c072001f7c8ef36a63
taowu750 <wutaodsg5201314@gmail.com>
2021-09-02 15:16:36 +0800

    :pencil2: 修改“22.[Medium] 括号生成”题解，增加链接。

--- src/training/backtracking/E22_Medium_GenerateParentheses.java
+++ src/training/backtracking/E22_Medium_GenerateParentheses.java
@@ -12,6 +12,8 @@ import static java.util.Collections.singletonList;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
+ * 22. 括号生成: https://leetcode-cn.com/problems/generate-parentheses/
+ *
  * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且「有效的」括号组合。
  *
  * 例 1：
