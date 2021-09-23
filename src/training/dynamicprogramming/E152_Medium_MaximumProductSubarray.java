8aa428cf10185cd335ee98cb1ccfe38dc0593010
taowu750 <wutaodsg5201314@gmail.com>
2021-09-23 20:18:05 +0800

    :heavy_plus_sign: 修改“152.[Medium] 乘积最大子数组”题解，新增滑动窗口算法。

--- src/training/dynamicprogramming/E152_Medium_MaximumProductSubarray.java
+++ src/training/dynamicprogramming/E152_Medium_MaximumProductSubarray.java
@@ -97,4 +97,59 @@ public class E152_Medium_MaximumProductSubarray {
     public void testDpMethod() {
         test(this::dpMethod);
     }
+
+
+    /**
+     * 滑动窗口方法。
+     *
+     * 0 与任何数的乘积都是 0，因此可以将数组看成被 0 分割的子数组，在各个子数组中查找乘积最大的值。
+     * 在一个非 0 的数组中求最大乘积，需要分析正负性。
+     * - 没有负数或者负数为偶数个，最大乘积就是整个数组的乘积
+     * - 有奇数个负数，如果第 i 个元素为负数，则 [start,i-1]，[i+1,end] 这2个区间的乘积都是最大乘积的候选。
+     *   通过下面 2 个指针交替移动算法可以计算所有 [start,i-1] 和 [i+1,end] 的乘积。
+     *
+     * 参见：
+     * https://leetcode-cn.com/problems/maximum-product-subarray/solution/shuang-zhi-zhen-by-wanglongjiang-ier1/
+     *
+     * LeetCode 耗时：0 ms - 100.00%
+     *          内存消耗：38.4 MB - 5.18%
+     */
+    public int slideWindowMethod(int[] nums) {
+        int left = 0, right = 0;
+        int result = Integer.MIN_VALUE;
+
+        int product = 1;
+        // 遇到 0 左指针开始收敛
+        while (right <= nums.length && left < nums.length) {
+            //如果右指针到达最后一个位置，左指针开始滑动
+            if (right == nums.length) {
+                product /= nums[left++];
+            } else {
+                // 如果右指针所指元素不为 0，右滑，更新 product
+                if (nums[right] != 0) {
+                    product *= nums[right++];
+                } else if (left < right) {  // 右指针为0，开始左滑，更新 product
+                    product /= nums[left++];
+                } else { // 左右指针都滑到了 0 的位置，更新 result，左右指针指向下一位置重新滑动
+                    if (result < 0) {
+                        result = 0;
+                    }
+                    left++;
+                    right++;
+                }
+            }
+            // 只要左右指针没有指向同一位置，更新 result（指向同一位置说明左滑到顶了，记录的 product 一定为1，
+            // 但并不是乘积值）
+            if (left != right && product > result) {
+                result = product;
+            }
+        }
+
+        return result;
+    }
+
+    @Test
+    public void testSlideWindowMethod() {
+        test(this::slideWindowMethod);
+    }
 }
