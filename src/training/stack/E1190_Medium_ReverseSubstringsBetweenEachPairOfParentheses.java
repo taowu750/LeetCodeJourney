76a33c6e430509b62ebf1f5135dfd42fb8b8e820
taowu750 <wutaodsg5201314@gmail.com>
2021-10-31 15:12:28 +0800

    :sparkles: 新增“1190.[Medium] 反转每对括号间的子串”题解。

---
+++ src/training/stack/E1190_Medium_ReverseSubstringsBetweenEachPairOfParentheses.java
@@ -0,0 +1,86 @@
+package training.stack;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.function.UnaryOperator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * 1190. 反转每对括号间的子串: https://leetcode-cn.com/problems/reverse-substrings-between-each-pair-of-parentheses/
+ *
+ * 给出一个字符串 s（仅含有小写英文字母和括号）。
+ * 请你按照从括号内到外的顺序，逐层反转每对匹配括号中的字符串，并返回最终的结果。
+ * 注意，您的结果中「不应」包含任何括号。
+ *
+ * 例 1：
+ * 输入：s = "(abcd)"
+ * 输出："dcba"
+ *
+ * 例 2：
+ * 输入：s = "(u(love)i)"
+ * 输出："iloveu"
+ * 解释：先反转子字符串 "love" ，然后反转整个字符串。
+ *
+ * 例 3：
+ * 输入：s = "(ed(et(oc))el)"
+ * 输出："leetcode"
+ * 解释：先反转子字符串 "oc" ，接着反转 "etco" ，然后反转整个字符串。
+ *
+ * 例 4：
+ * 输入：s = "a(bcdefghijkl(mno)p)q"
+ * 输出："apmnolkjihgfedcbq"
+ *
+ * 约束：
+ * - 0 <= s.length <= 2000
+ * - s 中只有小写英文字母和括号
+ * - 题目测试用例确保所有括号都是成对出现的
+ */
+public class E1190_Medium_ReverseSubstringsBetweenEachPairOfParentheses {
+
+    public static void test(UnaryOperator<String> method) {
+        assertEquals("dcba", method.apply("(abcd)"));
+        assertEquals("iloveu", method.apply("(u(love)i)"));
+        assertEquals("leetcode", method.apply("(ed(et(oc))el)"));
+        assertEquals("apmnolkjihgfedcbq", method.apply("a(bcdefghijkl(mno)p)q"));
+        assertEquals("", method.apply(""));
+        assertEquals("", method.apply("((()))"));
+        assertEquals("abc", method.apply("((abc))"));
+    }
+
+    /**
+     * LeetCode 耗时：1 ms - 98.17%
+     *          内存消耗：36.4 MB - 71.34%
+     */
+    public String reverseParentheses(String s) {
+        char[] chars = new char[s.length()];
+        int end = 0;
+        // stack 记录每个左括号包围字符序列在 chars 中的起始下标
+        Deque<Integer> stack = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(end);
+            } else if (c == ')') {
+                // 反转括号区域
+                int start = stack.pop(), half = (end - start) / 2;
+                for (int j = start, k = 1; k <= half; j++, k++) {
+                    char tmp = chars[j];
+                    chars[j] =  chars[end - k];
+                    chars[end - k] = tmp;
+                }
+            } else {
+                chars[end++] = c;
+            }
+        }
+
+        return new String(chars, 0, end);
+    }
+
+    @Test
+    public void testReverseParentheses() {
+        test(this::reverseParentheses);
+    }
+}
