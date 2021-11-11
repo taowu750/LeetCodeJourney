6c40b1c7a7a8470472822a5e76fd694e0d8dc960
taowu750 <wutaodsg5201314@gmail.com>
2021-11-11 15:34:14 +0800

    :sparkles: 新增“E443.[Medium] 压缩字符串”题解。

---
+++ src/training/string/E443_Medium_StringCompression.java
@@ -0,0 +1,91 @@
+package training.string;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.ToIntFunction;
+
+/**
+ * 443. 压缩字符串: https://leetcode-cn.com/problems/string-compression/
+ *
+ * 给你一个字符数组 chars ，请使用下述算法压缩：
+ * 从一个空字符串 s 开始。对于 chars 中的每组「连续重复字符」：
+ * - 如果这一组长度为 1 ，则将字符追加到 s 中。
+ * - 否则，需要向 s 追加字符，后跟这一组的长度。
+ *
+ * 压缩后得到的字符串 s 不应该直接返回 ，需要转储到字符数组 chars 中。需要注意的是，
+ * 如果组长度为 10 或 10 以上，则在 chars 数组中会被拆分为多个字符。
+ *
+ * 请在「修改完输入数组后」，返回该数组的新长度。
+ *
+ * 你必须设计并实现一个只使用常量额外空间的算法来解决此问题。
+ *
+ * 例 1：
+ * 输入：chars = ["a","a","b","b","c","c","c"]
+ * 输出：返回 6 ，输入数组的前 6 个字符应该是：["a","2","b","2","c","3"]
+ * 解释：
+ * "aa" 被 "a2" 替代。"bb" 被 "b2" 替代。"ccc" 被 "c3" 替代。
+ *
+ * 例 2：
+ * 输入：chars = ["a"]
+ * 输出：返回 1 ，输入数组的前 1 个字符应该是：["a"]
+ * 解释：
+ * 没有任何字符串被替代。
+ *
+ * 例 3：
+ * 输入：chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+ * 输出：返回 4 ，输入数组的前 4 个字符应该是：["a","b","1","2"]。
+ * 解释：
+ * 由于字符 "a" 不重复，所以不会被压缩。"bbbbbbbbbbbb" 被 “b12” 替代。
+ * 注意每个数字在数组中都有它自己的位置。
+ *
+ * 说明：
+ * - 1 <= chars.length <= 2000
+ * - chars[i] 可以是小写英文字母、大写英文字母、数字或符号
+ */
+public class E443_Medium_StringCompression {
+
+    public static void test(ToIntFunction<char[]> method) {
+        Assertions.assertEquals(6, method.applyAsInt(new char[]{'a','a','b','b','c','c','c'}));
+        Assertions.assertEquals(1, method.applyAsInt(new char[]{'a'}));
+        Assertions.assertEquals(4, method.applyAsInt(new char[]{'a','b','b','b','b','b','b','b','b','b','b','b','b'}));
+    }
+
+    /**
+     * LeetCode 耗时：1 ms - 94.43%
+     *          内存消耗：38.2 MB - 31.14%
+     */
+    public int compress(char[] chars) {
+        // 用 idx 表示实际字符写入的位置，i 表示读取的位置
+        int idx = 0, cnt = 1;
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] == chars[i - 1]) {
+                cnt++;
+            } else {
+                chars[idx++] = chars[i - 1];
+                if (cnt > 1) {
+                    String cntStr = String.valueOf(cnt);
+                    for (int j = 0; j < cntStr.length(); j++) {
+                        chars[idx++] = cntStr.charAt(j);
+                    }
+                }
+                cnt = 1;
+            }
+        }
+        // 别忘了处理最后一个字符
+        chars[idx++] = chars[chars.length - 1];
+        if (cnt > 1) {
+            String cntStr = String.valueOf(cnt);
+            for (int j = 0; j < cntStr.length(); j++) {
+                chars[idx++] = cntStr.charAt(j);
+            }
+        }
+
+        return idx;
+    }
+
+    @Test
+    public void testCompress() {
+        test(this::compress);
+    }
+}
