860d64c12a09041be89147e4e7ffc61e92d41987
taowu750 <wutaodsg5201314@gmail.com>
2022-04-04 15:16:36 +0800

    :pencil: 修改“G001 (a^b)%p”题解。

--- src/acguide/_0x00_basicalgorithm/_0x01_bitwise/G001_APowB.java
+++ src/acguide/_0x00_basicalgorithm/_0x01_bitwise/G001_APowB.java
@@ -6,7 +6,7 @@ import util.datastructure.function.TriIntOperator;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * https://ac.nowcoder.com/acm/contest/996/A
+ * a^b%p：https://ac.nowcoder.com/acm/contest/996/A
  *
  * 求 a 的 b 次方对 p 取模的值，其中 0 <= a,b,p <= 10^9, p > 0
  */
@@ -14,17 +14,18 @@ public class G001_APowB {
 
     public static void test(TriIntOperator method) {
         assertEquals(8, method.applyAsInt(2, 3, 9));
+        assertEquals(0, method.applyAsInt(1, 1, 1));
     }
 
     public int power(int a, int b, int p) {
-        long aPowB = 1;
-        for (long aPow = a; b > 0; b >>= 1, aPow = (aPow * aPow) % p) {
+        long aPowB = 1 % p;
+        for (long aPow = a; b > 0; b >>= 1, aPow = aPow * aPow % p) {
             if ((b & 1) == 1) {
-                aPowB = (aPowB * aPow) % p;
+                aPowB = aPowB * aPow % p;
             }
         }
 
-        return (int) (aPowB % p);
+        return (int) aPowB;
     }
 
     @Test
