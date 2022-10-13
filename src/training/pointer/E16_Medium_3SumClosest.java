205dba98560b634775af3d5b0f8927f8c9a51e0e
taowu750 <wutaodsg5201314@gmail.com>
2022-10-13 10:01:19 +0800

    :pencil2: 复习题解。

--- src/training/pointer/E16_Medium_3SumClosest.java
+++ src/training/pointer/E16_Medium_3SumClosest.java
@@ -31,25 +31,54 @@ public class E16_Medium_3SumClosest {
     }
 
     /**
-     * LeetCode 耗时：5 ms - 90.08%
-     *          内存消耗：38.1 MB - 61.25%
+     * 加入了很多剪枝操作，大大减少了耗时
+     *
+     * LeetCode 耗时：6 ms - 99.40%
+     *          内存消耗：41.3 MB - 76.71%
      */
     public int threeSumClosest(int[] nums, int target) {
+        final int n = nums.length;
+        // 其实还可以用计数排序进一步加快速度
         Arrays.sort(nums);
 
         int result = nums[0] + nums[1] + nums[2];
-        for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = i + 1, k = nums.length - 1; j < k;) {
-                int num = nums[i] + nums[j] + nums[k];
+        for (int i = 0; i < n - 2; i++) {
+            // 如果最小和都大于等于 target，那后面就无需遍历了
+            int minSum = nums[i] + nums[i + 1] + nums[i + 2];
+            if (minSum >= target) {
+                if (minSum - target < Math.abs(result - target)) {
+                    result = minSum;
+                }
+                break;
+            }
+
+            for (int l = i + 1, r = nums.length - 1; l < r;) {
+                minSum = nums[i] + nums[l] + nums[l + 1];
+                if (minSum >= target) {
+                    if (minSum - target < Math.abs(result - target)) {
+                        result = minSum;
+                    }
+                    break;
+                }
+                // 如果最大和都小于等于 target，那后面就无需遍历了
+                int maxSum = nums[i] + nums[r - 1] + nums[r];
+                if (maxSum <= target) {
+                    if (target - maxSum < Math.abs(result - target)) {
+                        result = maxSum;
+                    }
+                    break;
+                }
+
+                int num = nums[i] + nums[l] + nums[r];
                 if (Math.abs(num - target) < Math.abs(result - target)) {
                     result = num;
                 }
                 if (num == target) {
                     return target;
                 } else if (num > target) {
-                    k--;
+                    r--;
                 } else {
-                    j++;
+                    l++;
                 }
             }
         }
